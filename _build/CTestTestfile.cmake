9c468e833febb0148da2c98c27ba56045bd81fc2
nnnnnmmmmm092 <nnnnnmmmmm092@protonmail.com>
2022-04-13 13:13:49 +0300

    half of the tests

---
+++ _build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/augustus/TIMP/lab05
+# Build directory: /home/augustus/TIMP/lab05/_build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(check "/home/augustus/TIMP/lab05/_build/check")
+set_tests_properties(check PROPERTIES  _BACKTRACE_TRIPLES "/home/augustus/TIMP/lab05/CMakeLists.txt;17;add_test;/home/augustus/TIMP/lab05/CMakeLists.txt;0;")
+subdirs("third-party/gtest")
